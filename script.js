6ce4d82f79e4b3b1e4319201ff5ba69b868bffc1
Praveenkum12 <praveenkum1225@gmail.com>
2023-07-23 14:24:30 +0530

    second commit

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 
 const airline = 'TAP Air Portugal';
 console.log(airline.slice(4, 7));
-console.log();
+console.log(airline.toUpperCase());
+console.log(airline.toLowerCase());
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
