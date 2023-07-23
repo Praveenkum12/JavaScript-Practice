8a94157d3e6b944d25e8460cefe51ccdebefa0ba
Praveenkum12 <praveenkum1225@gmail.com>
2023-07-23 14:47:33 +0530

    change

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
 
 const airline = 'TAP Air Portugal';
 console.log(airline.slice(4, 7));
+console.log(airline.slice(-2));
 console.log(airline.toUpperCase());
 console.log(airline.toLowerCase());
 console.log(airline.indexOf('r'));
 console.log(airline.lastIndexOf('r'));
+console.log(airline.includes('Air'));
+console.log(airline.split(' '));
+console.log(airline.startsWith('T'));
+console.log(airline.length);
+console.log(airline.padStart(20, '*'));
+console.log(airline.padEnd(20, '*'));
+
+const passenger = 'pRAveen';
+const passengerLower = passenger.toLowerCase();
+const [first, ...others] = [...passengerLower];
+console.log([first.toUpperCase(), others].flat().join(''));
